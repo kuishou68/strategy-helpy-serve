e2f46c022ede49d35ff9b11e144fb01b5e893344
lijianlin <17607650602@shboka.onaliyun.com>
2023-06-15 18:03:19 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,25 @@
+# 策略助手server服务，基于Nest.js + typeorm + mySql + swagger
+
+## 启动流程
+
+1. 正常的 Nest.js 项目启动开发流程
+
+   ````css
+   npm i
+   npm run start:dev
+   ````
+
+2. git 提交
+
+   ````javascript
+   git add .
+   git commit -m "feat：***";
+   git remote add origin https://github.com/kuishou68/strategy-helpy-server.git
+   git push -u origin main
+   ````
+
+3. 预览
+```javascript
+   http://127.0.0.1:9080/docs
+```
+
---
+++ src/app.module.ts
@@ -0,0 +1,47 @@
+/**
+ * 根模块提供了用来启动应用的引导机制
+ * providers：Nest.js注入器实例化的提供者（服务提供者），处理具体的业务逻辑，各个模块之间可以共享（注入器的概念后面依赖注入部分会讲解）；
+ * controllers：处理http请求，包括路由控制，向客户端返回响应，将具体业务逻辑委托给providers处理；
+ * imports：导入模块的列表，如果需要使用其他模块的服务，需要通过这里导入；
+ * exports：导出服务的列表，供其他模块导入使用。如果希望当前模块下的服务可以被其他模块共享，需要在这里配置导出；
+ */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import envConfig from '../config/env';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
+import { TagModule } from './tag/tag.module';
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: [envConfig.path] }),
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST', '127.0.0.0'),
+        port: configService.get<number>('DB_PORT', 3306),
+        username: configService.get('DB_USER', 'root'),
+        password: configService.get('DB_PASSWORD', 'root'),
+        database: configService.get('DB_DATABASE', 'blog'),
+        // charset: 'utf8mb4',
+        timezone: '+08:00',
+        synchronize: true,
+        autoLoadEntities: true
+      }),
+    }),
+    PostsModule,
+    UserModule,
+    AuthModule,
+    CategoryModule,
+    TagModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
